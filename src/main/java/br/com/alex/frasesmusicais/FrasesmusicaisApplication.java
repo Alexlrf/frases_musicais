f1ca6b584253f1cd61f6d123398cfd4b54d1a12b
AlexFogazzi <alex.fogazzi.dev@gmail.com>
2024-04-19 22:56:01 -0300

    deploy

--- src/main/java/br/com/alex/frasesmusicais/FrasesmusicaisApplication.java
+++ src/main/java/br/com/alex/frasesmusicais/FrasesmusicaisApplication.java
@@ -2,8 +2,10 @@ package br.com.alex.frasesmusicais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class FrasesmusicaisApplication {
 
 	public static void main(String[] args) {
