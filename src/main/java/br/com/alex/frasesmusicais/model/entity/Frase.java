3578020490cb749ae1e05c19a9fafa903bab6559
Alex <alexrfogazzi@gmail.com>
2024-10-29 22:28:38 -0300

    Refactor: inclui construtores para fazer a conversao dtos x entities

--- src/main/java/br/com/alex/frasesmusicais/model/entity/Frase.java
+++ src/main/java/br/com/alex/frasesmusicais/model/entity/Frase.java
@@ -6,15 +6,23 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Optional;
+
+import br.com.alex.frasesmusicais.model.dto.ArtistaDTO;
+import br.com.alex.frasesmusicais.model.dto.FraseDTO;
 
 @Entity
 @Data
 @EqualsAndHashCode
+@NoArgsConstructor
 public class Frase implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_frase")
     private Long idFrase;
@@ -36,4 +44,15 @@ public class Frase implements Serializable {
     @JoinColumn(name = "id_artista")
     @ManyToOne(cascade=CascadeType.PERSIST)
     private Artista artista;
+    
+    public Frase(FraseDTO fraseDto) {
+    	if(Optional.ofNullable(fraseDto.getIdFrase()).isPresent()) {
+    		this.idFrase = fraseDto.getIdFrase();
+    	}
+    	this.texto = fraseDto.getTexto();
+    	this.nomeMusica = fraseDto.getNomeMusica();
+    	this.linkVideo = fraseDto.getLinkVideo();
+    	this.artista = new Artista(fraseDto.getArtista());
+    }
+    
 }
